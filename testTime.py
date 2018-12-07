426c82db08f237aebd898f8d0570d185faa6afbc
Joe Wang <joewwwang1126@gmail.com>
2018-12-07 15:19:18 +0800

    Update testTime.py

--- testTime.py
+++ testTime.py
@@ -1,8 +1,5 @@
 import time
 class TimeDemo:
-
-
     def ppp(self):
         localtime = time.asctime(time.localtime(time.time()))
         return localtime
-# print(isinstance(TimeDemo.ppp(),str))
